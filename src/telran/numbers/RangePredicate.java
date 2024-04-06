9c60d201ccadf35dd60c245dd0e3df6f8afef549
DmitriyVolchenko <Dmitriy.Volchenko@gmail.com>
2024-04-06 16:08:43 +0300

    changes code to avoid changing state of current in hasNext

--- src/telran/numbers/RangePredicate.java
+++ src/telran/numbers/RangePredicate.java
@@ -5,46 +5,53 @@ import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 
 public class RangePredicate extends Range{
-	private Predicate<Integer> predicate;
-	
-	protected RangePredicate(int min, int max) {
-		super(min, max);		
-	}
-	
-	public void setPredicate(Predicate<Integer> predicate) {
-		this.predicate = predicate;
-	}
-	
-	public static RangePredicate getRange(int min, int max) {
-		checkMinMax(min, max);
-		return new RangePredicate(min, max);
-	}
-	@Override
-	public Iterator<Integer> iterator() {
-		return new RangePredicateIterator();
-	}
-	private class RangePredicateIterator implements Iterator<Integer> {
-		  private int current = min;
-		@Override
-		public boolean hasNext() {
-			boolean isNext = false;
-			 while (current <= max&&!isNext) {
-	                if (predicate == null || predicate.test(current)) 
-	                    isNext=true;                
-	                else current++;
-	            }
-	            return isNext;
-		}
+    private Predicate<Integer> predicate;
+    
+    protected RangePredicate(int min, int max, Predicate<Integer> predicate) {
+        super(min, max);   
+        this.predicate = predicate;
+    }
+    
+    public static RangePredicate getRange(int min, int max, Predicate<Integer> predicate) {
+        checkMinMax(min, max);
+        return new RangePredicate(min, max, predicate);//adds state of predicate to instance
+    }
+    
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RangePredicateIterator();
+    }
+    
+    private class RangePredicateIterator implements Iterator<Integer> {
+        private int current = min;
+        
+        public RangePredicateIterator() {
+            findNext(); // finding next element without changing state of current in hasNext
+        }
+        
+        @Override
+        public boolean hasNext() {
+            return current <= max;
+        }
 
-		@Override
-		public Integer next() {
-			   if (!hasNext()) {
-	                throw new NoSuchElementException();
-	            }
-	            return current++;
-		}
-		
-		
-	}
-
+        @Override
+        public Integer next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            int next = current;
+            current++;
+            findNext(); 
+            return next;
+        }
+        
+        private void findNext() {
+            while (current <= max) {
+                if (predicate == null || predicate.test(current)) {
+                    return;
+                }
+                current++;
+            }
+        }
+    }
 }
--- src/telran/numbers/test/RangePredicateTest.java
+++ src/telran/numbers/test/RangePredicateTest.java
@@ -13,26 +13,17 @@ class RangePredicateTest extends RangeTest{
 
 	@Test
 	void iterableIteratorTest() {
-		RangePredicate range = RangePredicate.getRange(10, 15);
-		Integer[] rangeExpected = {10, 11, 12, 13, 14, 15};
-		assertArrayEquals(rangeExpected, toArrayFromIterable(new Integer[rangeExpected.length],
-				range));
-		RangePredicate rangeEvenOdd = RangePredicate.getRange(1, 7);
-		Integer[] rangeOddExpected = {1, 3, 5, 7};
-		Integer[] rangeEvenExpected = {2, 4, 6};
-		rangeEvenOdd.setPredicate(n -> n % 2 != 0);
-		assertArrayEquals(rangeOddExpected, toArrayFromIterable(new Integer[rangeOddExpected.length],
-				rangeEvenOdd));
-		rangeEvenOdd.setPredicate(n -> n % 2 == 0);
-		assertArrayEquals(rangeEvenExpected, toArrayFromIterable(new Integer[rangeEvenExpected.length],
-				rangeEvenOdd));
+	    RangePredicate range = RangePredicate.getRange(10, 15, null); // Передайте null в качестве предиката
+	    Integer[] rangeExpected = {10, 11, 12, 13, 14, 15};
+	    assertArrayEquals(rangeExpected, toArrayFromIterable(new Integer[rangeExpected.length], range));
+	    
+	    // Теперь проверяем на четные и нечетные числа с использованием предиката
+	    RangePredicate rangeEven = RangePredicate.getRange(1, 7, n -> n % 2 == 0); // Передайте предикат для четных чисел
+	    Integer[] rangeEvenExpected = {2, 4, 6};
+	    assertArrayEquals(rangeEvenExpected, toArrayFromIterable(new Integer[rangeEvenExpected.length], rangeEven));
+	    
+	    RangePredicate rangeOdd = RangePredicate.getRange(1, 7, n -> n % 2 != 0); // Передайте предикат для нечетных чисел
+	    Integer[] rangeOddExpected = {1, 3, 5, 7};
+	    assertArrayEquals(rangeOddExpected, toArrayFromIterable(new Integer[rangeOddExpected.length], rangeOdd));
 	}
-	@Test
-	void iteratorIncorrectUsageTest() {
-		RangePredicate rangePredicate = RangePredicate.getRange(1, 9);
-		rangePredicate.setPredicate(n -> n % 10 == 0);
-		Iterator<Integer> it = rangePredicate.iterator();
-		assertThrowsExactly(NoSuchElementException.class, ()->it.next());
-	}
-
 }
